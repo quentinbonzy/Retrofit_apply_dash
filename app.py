5c4145c359dc5643a25ec65b682b8f580305640c
quentinbonzy <quentin.bonzy.external@airbus.com>
2026-08-20 10:51:22 +0200

    Initialize Dash application with Bootstrap theme

---
+++ app.py
@@ -0,0 +1,16 @@
+import dash
+import dash_bootstrap_components as dbc
+
+# 1. Création de l'application avec le thème Bootstrap
+app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
+
+# 2. Import de l'interface
+from coef_management import layout
+
+# 3. Assignation
+app.layout = layout()
+
+if __name__ == '__main__':
+    # Remplacé par app.run avec le host pour autoriser TSAS à se connecter
+    app.run(host='0.0.0.0', port=9036, debug=False)
